d2d58cfc254cee4d3e28919db05be0925cf7af11
CtrlMagno <jonnathanesmen3@gmail.com>
2025-03-28 18:26:35 -0500

    6Commit

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,7 +1,17 @@
 class CardComponent extends HTMLElement {
 
-    
+    static get observedAttributes() {
+        return ['img','tittle', 'text', 'review', 'price','state'];
+    }
 
+    constructor(){
+        super();
+    };
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if(oldValue !== newValue)
+            name.this;
 
 }
 
+customElements.define("card-component", CardComponent)
